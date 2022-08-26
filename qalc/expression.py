d0102bb7b50c38825fb2a1d82ae1bfac49a00a61
Ederic Oytas <edericoytas@gmail.com>
2022-08-26 15:31:06 -0700

    Create expression.py

---
+++ qalc/expression.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+
+class Expression:
+    """Abstract base class for all expressions in qalc."""
+
+    @abstractmethod
+    def evaluate(self) -> Any:
+        """Return the value of this expression."""
+
+
+@dataclass
+class Addition:
+    """Represents an addition operation (+)."""
+    left: Expression
+    right: Expression
+    
+    def evaluate(self) -> Any:
+        return self.left.evaluate() + self.right.evaluate()
+
+
+@dataclass
+class Number:
+    """Represents a number in qalc."""
+    value: float
+    
+    def evaluate(self) -> float:
+        return self.value
